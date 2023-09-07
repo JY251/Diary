ac61719c4cf338b53df28f06da9aab01c0d53926
JY251 <101054474+JY251@users.noreply.github.com>
2023-09-08 01:00:45 +0900

    initial version of the website

---
+++ README.md
@@ -0,0 +1,10 @@
+# README
+
+- `app.py`: server.
+- `index.html`:
+
+## Colab only
+- `run.py`: run the server on colab
+
+## Not used
+- `server.ipynb`: ipynb of `app.py` (mistakenly created but not used.)
---
+++ app.py
@@ -0,0 +1,25 @@
+# !pip install Flask
+# pip install Flask
+
+from flask import Flask, request, render_template
+import os
+
+
+app = Flask(__name__)
+
+diary_entries = {}
+
+@app.route('/')
+def index():
+  return render_template('index.html', entries=diary_entries)
+
+@app.route('/add_entry', methods=['POST'])
+def add_entry():
+  data = request.form['date']
+  entry = request.form['entry']
+  diary_entries[date] = entry
+  return render_template('index.html', entries=diary_entries)
+
+if __name__ == '__main__':
+  # app.run(debug=True)
+  app.run(host='0.0.0.0', port=5000)
